328d65829219711fe50ce987d5643dc7a0e21b70
황두리 <hwangduli0515@gmail.com>
2021-10-25 22:48:30 +0900

    first commit

---
+++ src/main/java/com/zmarket/zmarket/ZmarketApplication.java
@@ -0,0 +1,13 @@
+package com.zmarket.zmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zmarket/zmarket/ZmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmarket.zmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
