0a3ae0c834e108c20cdae672dd9fb5ae7fde3c80
황두리 <hdr@gabia.com>
2021-12-14 17:42:19 +0900

    카카오 로그인 추가

--- src/main/java/com/zmarket/zmarket/dto/OAuthAttributes.java
+++ src/main/java/com/zmarket/zmarket/dto/OAuthAttributes.java
@@ -27,8 +27,27 @@ public class OAuthAttributes {
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
         // 여기서 네이버와 카카오 등 구분 (ofNaver, ofKakao)
+        //(new!) kakao
+        if("kakao".equals(registrationId)){
+            return ofKakao("id", attributes);
+        }
 
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        // kakao는 kakao_account에 유저정보가 있다. (email)
+        Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
+        // kakao_account안에 또 profile이라는 JSON객체가 있다. (nickname, profile_image)
+        Map<String, Object> kakaoProfile = (Map<String, Object>)kakaoAccount.get("profile");
+
+        return OAuthAttributes.builder()
+                .name((String) kakaoProfile.get("nickname"))
+                .email((String) kakaoAccount.get("email"))
+                .picture((String) kakaoProfile.get("profile_image_url"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
